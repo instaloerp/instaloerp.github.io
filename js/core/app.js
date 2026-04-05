80b0f5c419159469c8b443b7155a96023d2f4b72
jordiinstalaciones <168295569+jordiinstalaciones@users.noreply.github.com>
2026-04-05 11:27:11 +0200

    Add files via upload

--- js/core/app.js
+++ js/core/app.js
@@ -632,6 +632,7 @@ async function mostrarApp() {
     localStorage.setItem('sb_favoritos', JSON.stringify(sbFavoritos));
   }
   renderFavoritos();
+  document.getElementById('pgTitle').textContent = '🏠 Panel';
   document.getElementById('pgSub').textContent = _fechaHoraActual();
   document.getElementById('tr_fecha').value = new Date().toISOString().split('T')[0];
   // Cargar datos
